5748b0341cf56de2007692d0ef6fb5e96ec8984b
andrey <scream12346@yandex.ru>
2024-04-13 20:29:05 +0300

    Initial commit

---
+++ src/main/java/com/kamenskiy/io/App.java
@@ -0,0 +1,21 @@
+package com.kamenskiy.io;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080).addService(new GreetingServiceImpl()).build();
+        server.start();
+        server.awaitTermination();
+        System.out.println("Server started");
+
+    }
+}
